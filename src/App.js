b0cb41d01bee163deaebb0e8827e617382baab9f
bonusloop <denis7sined@gmail.com>
2021-08-25 20:55:41 +0300

    uffTest3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import Headlaine from './components/headlaine/Headlaine';
 
+import img1 from './components/assets/image/image1.jpg';
+import img2 from './components/assets/image/image2.jpg';
+import img3 from './components/assets/image/image3.jpg';
+import img4 from './components/assets/image/image4.jpg';
+import img5 from './components/assets/image/image5.jpg';
+import img6 from './components/assets/image/image6.jpg';
+
 function App() {
   return (
     <div className="App">
-      <Headlaine/>
-      <Headlaine/>
-      <Headlaine/>
-      <Headlaine/>
-      <Headlaine/>
-      <Headlaine/>
+      <div className="wrapper">
+        <Headlaine thumbnail={img1} time="2:00 pm" like="10.8k" dislake="1.4k" label="BBC NEWS" title="Hate speech vs free speech"/>
+        <Headlaine thumbnail={img2} time="2:00 pm" like="10.8k" dislake="3.4k" label="BBC NEWS" title="Ontario Liberal leadership"/>
+        <Headlaine thumbnail={img3} time="2:00 pm" like="20.8k" dislake="1.1k" label="BBC NEWS" title="Victim of Scarborough"/>
+        <Headlaine thumbnail={img4} time="2:00 pm" like="0" dislake="20k" label="ABC NEWS" title="Mother who left her baby"/>
+        <Headlaine thumbnail={img5} time="2:00 pm" like="10.8k" dislake="1.4k" label="CW6 NEWS" title="Mother of Stefanie Rengel"/>
+        <Headlaine thumbnail={img6} time="2:00 pm" like="10.8k" dislake="1.4k" label="CNN NEWS" title="World Cup: Popular items"/>
+      </div>
     </div>
   );
 }
--- src/components/headlaine/Headlaine.js
+++ src/components/headlaine/Headlaine.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import './Headlaine.css';
-import thumbnail from '../assets/image/image1.jpg';
+import s from'./Headlaine.module.css';
+
 import like from '../assets/image/like.svg';
 import dislike from '../assets/image/dislike.svg';
 import time from '../assets/image/time.svg';
 
-function Headlaine() {
+function Headlaine(props) {
   return (
-    <article className="headline">
-        <img className="thumbnail-img" src={thumbnail} alt="" />
-          <div className="info">
-            <h1 className="title" >Hate speech vs free speech</h1>
-            <div className="rating">
-              <div className="like">
-                <img className="like-icon" src={like} alt="" />
-                <span className="text" >10.8k</span>
+    <article className={s.headline}>
+        <img className={s.thumbnailImg} src={props.thumbnail} alt="" />
+          <div className={s.info}>
+            <h1 className={s.title}>{props.title}</h1>
+            <div className={s.rating}>
+              <div className={s.like}>
+                <img className={s.likeIcon} src={like} alt="" />
+                <span className={s.text}>{props.like}</span>
               </div>
-              <div className="dislike">
-                <img className="dislike-icon" src={dislike} alt="" />
-                <span className="text" >1.4k</span>
+              <div className={s.dislike}>
+                <img className={s.dislikeIcon} src={dislike} alt="" />
+                <span className={s.text}>{props.dislake}</span>
               </div>
             </div>
           
-            <div className="wrapper">
-              <span className="label" >BBC NEWS</span>
-                <div className="time">
-                  <img className="time-icon" src={time} alt="" />
-                  <span className="time-text" >2:00 pm</span>
+            <div className={s.wrapper}>
+              <span className={s.label}>{props.label}</span>
+                <div className={s.time}>
+                  <img className={s.timeIcon} src={time} alt="" />
+                  <span className={s.timeText}>{props.time}</span>
                 </div>
             </div>
           </div>
